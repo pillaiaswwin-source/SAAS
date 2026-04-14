0e904ba459d70f1a43159327d3758bc5d4ed16cc
pillaiaswwin-source <pillaiaswwin@gmail.com>
2026-04-14 17:21:25 +0530

    Update Firebase configuration and initialization

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
-const firebaseConfig={apiKey:"YOUR_KEY"};firebase.initializeApp(firebaseConfig);
+// Firebase Config (YOUR DATA)
+const firebaseConfig = {
+  apiKey: "AIzaSyDpqPoVGutCKRPyP4ixsoA7zqGN9L6jqYk",
+  authDomain: "container-saas.firebaseapp.com",
+  projectId: "container-saas",
+  storageBucket: "container-saas.firebasestorage.app",
+  messagingSenderId: "830981793717",
+  appId: "1:830981793717:web:8a890c0e6af45b6df3dbfc",
+  measurementId: "G-MGP35KZEWW"
+};
+
+// Initialize Firebase (v8 style)
+firebase.initializeApp(firebaseConfig);
+
+// Services
+const auth = firebase.auth();
+const db = firebase.firestore();
